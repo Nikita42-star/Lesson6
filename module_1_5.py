34dfd556aa5e1a906ebc91d276b863d315606af2
Никита <n.s.galeev@gmail.com>
2024-10-07 15:49:00 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+# Не изменяемые
+immutable_var = 1, 2, 3, True, 'Summer'
+print(immutable_var)
+mmutable_var [4] = 'Winter'
+print(immutable_var)
+#Изменяемые
+mutable_list = ['Nikita', 567, 200, 3]
+print(mutable_list)
+mutable_list [0] = 'Anton'
+print(mutable_list)
